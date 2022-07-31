f7a239625abe9ca3f160154db2f23fa0d16b39f2
unknown <ammugayathri568@gmail.com>
2022-07-31 19:21:15 +0530

    first commit

---
+++ audit-authorization/target/classes/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (1, 'Ammu Gayathri', 'ag', 'ag000', 'Project-1');
+INSERT INTO project_manager_details(id, name, username, password, project_name) VALUES (2, 'Naveen Kumar', 'nk', 'nk000', 'Project-2');
